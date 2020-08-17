4cdd593a124dfac219eb2fb9a1cb9ea8ea4fb275
Dirk Lembke <dirk@lembke.nz>
2020-08-17 21:46:55 +1200

    build other platforms

--- build.sh
+++ build.sh
@@ -19,6 +19,14 @@ fi
 
 cd `go env GOPATH`/src/github.com/zauberhaus/rest2dhcp
 
-GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -ldflags "-X main.gitCommit=$COMMIT -X main.buildTime=$NOW -X main.treeState=$STATE -X main.tag=$TAGS $FLAGS" -o $OUT
+GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -ldflags "-X main.gitCommit=$COMMIT -X main.buildTime=$NOW -X main.treeState=$STATE -X main.tag=$TAGS $FLAGS" -o $OUT-amd64
+which upx && `which upx` $OUT-amd64
 
-which upx && `which upx` $OUT
+GOOS=linux GOARCH=arm64 CGO_ENABLED=0 go build -ldflags "-X main.gitCommit=$COMMIT -X main.buildTime=$NOW -X main.treeState=$STATE -X main.tag=$TAGS" -o $OUT-arm64
+which upx && `which upx` $OUT-arm64
+
+GOOS=linux GOARCH=arm CGO_ENABLED=0 go build -ldflags "-X main.gitCommit=$COMMIT -X main.buildTime=$NOW -X main.treeState=$STATE -X main.tag=$TAGS" -o $OUT-arm
+which upx && `which upx` $OUT-arm
+
+GOOS=linux GOARCH=386 CGO_ENABLED=0 go build -ldflags "-X main.gitCommit=$COMMIT -X main.buildTime=$NOW -X main.treeState=$STATE -X main.tag=$TAGS" -o $OUT-386
+which upx && `which upx` $OUT-386
