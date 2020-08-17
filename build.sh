5a061efee8896a265d826f5775c6bb8d3dbb42f9
Dirk Lembke <dirk@lembke.nz>
2020-08-17 17:30:23 +1200

    use docker to build executable

--- build.sh
+++ build.sh
@@ -5,10 +5,20 @@ TAGS=`git describe --tags 2> /dev/null`
 NOW=`date +%FT%T.%3N%:z`
 COMMIT=`git rev-parse HEAD`
 
-if test ! -z "$DIFF" ; then
+UPX=`which upx`
+
+if test ! -z "$DIFF"; then
   STATE='dirty'
 else
   STATE='clean'
 fi
 
-GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -ldflags "-X main.gitCommit=$COMMIT -X main.buildTime=$NOW -X main.treeState=$STATE -X main.tag=$TAGS"
+if test -z "$OUT"; then
+  OUT="./rest2dhcp"
+fi
+
+cd `go env GOPATH`/src/github.com/zauberhaus/rest2dhcp
+
+GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -ldflags "-X main.gitCommit=$COMMIT -X main.buildTime=$NOW -X main.treeState=$STATE -X main.tag=$TAGS" -o $OUT
+
+which upx && `which upx` $OUT
