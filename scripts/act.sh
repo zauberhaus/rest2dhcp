e0123b83ad9383a39fe4b60642d0191e15cad1d1
Dirk Lembke <dirk@lembke.nz>
2020-08-20 21:52:58 +1200

    clean up

--- scripts/act.sh
+++ scripts/act.sh
@@ -1,4 +1,3 @@
 #! /bin/sh
 
-cd ..
-act -P ubuntu-latest=nektos/act-environments-ubuntu:18.04 -j buildx -s DOCKER_USERNAME=zauberhaus -s DOCKER_TOKEN=E52NeYW2Pb8S
+act -P ubuntu-latest=nektos/act-environments-ubuntu:18.04 -j tests
