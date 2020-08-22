573291b86388cbe5c01c6d5c56823d5ba1697ef2
Dirk <dil@air13.fritz.box>
2020-08-22 18:55:31 +1200

    install semver and esc if not installed

--- build.sh
+++ build.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
+if ! which esc1 > /dev/null ; then 
+  P=`pwd`
+  cd ..
+  go get github.com/mjibson/esc
+  go get github.com/mdomke/git-semver
+  cd "$P"
+fi
+
 DIFF=`git diff --stat`
-TAGS=`git describe --tags 2> /dev/null`
+VERSION=`git semver -prefix v`
 NOW=`date +%FT%T.%3N%:z`
 COMMIT=`git rev-parse --short HEAD`
 
@@ -13,6 +21,7 @@ else
   STATE='clean'
 fi
 
+
 go generate ./...
-CGO_ENABLED=0 go build -ldflags "-X main.gitCommit=$COMMIT -X main.buildTime=$NOW -X main.treeState=$STATE -X main.tag=$TAGS $FLAGS"
+CGO_ENABLED=0 go build -ldflags "-X main.gitCommit=$COMMIT -X main.buildTime=$NOW -X main.treeState=$STATE -X main.tag=$VERSION $FLAGS"
 git describe --tags
